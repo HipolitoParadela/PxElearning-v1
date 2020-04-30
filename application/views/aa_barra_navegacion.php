d6effaf9a6aee809664bb52f485741dd4aec5874
Hipolito Paradela <31195000+HipolitoParadela@users.noreply.github.com>
2020-04-30 00:14:25 -0300

    Control

--- application/views/aa_barra_navegacion.php
+++ application/views/aa_barra_navegacion.php
@@ -27,11 +27,11 @@
                         <span></span>
                         <span></span>
                         <span></span>
-                    </div><!-- .hamburger-menu -->
+                    </div><!-- .hamburger-menu --><!-- 
 
                     <div class="header-bar-cart">
                         <a href="#" class="flex justify-content-center align-items-center"><span aria-hidden="true" class="icon_bag_alt"></span></a>
-                    </div><!-- .header-bar-search -->
+                    </div><!-- .header-bar-search --> 
                 </nav><!-- .site-navigation -->
             </div><!-- .col -->
         </div><!-- .row -->
